b42e19253c126952801a1135ea599945ec94e23e
HongJunHyeok <48292190+Hong-JunHyeok@users.noreply.github.com>
2021-03-23 09:04:13 +0900

    CREATE react app

---
+++ src/components/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="main">
+      <h1>커스텀 Hooks를 연습하는 연습장입니다.</h1>
+      <hr />
+    </div>
+  );
+}
+
+export default App;
