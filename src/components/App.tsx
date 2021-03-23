86bd05b24364387477f42305cfb11ac91b2c228f
HongJunHyeok <48292190+Hong-JunHyeok@users.noreply.github.com>
2021-03-23 09:37:25 +0900

    divide useInput Component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,22 +1,14 @@
 import React from 'react';
 import './App.css';
-import useInput from 'hooks/useInput';
+import UseInputCompo from './UseInputCompo';
 
 function App() {
-  const [form, formHandler] = useInput({
-    email: 'email',
-    password: 'password',
-  });
-
   return (
     <div className="main">
       <h1>커스텀 Hooks를 연습하는 연습장입니다.</h1>
       <hr />
       <h2>useInput</h2>
-      <form>
-        <input type="text" value={form.email} onChange={formHandler} id="email" />
-        <input type="password" value={form.password} onChange={formHandler} id="password" />
-      </form>
+      <UseInputCompo />
     </div>
   );
 }
---
+++ src/components/UseInputCompo.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import useInput from 'hooks/useInput';
+
+function UseInputCompo() {
+  const [form, formHandler] = useInput({
+    email: '',
+    password: '',
+  });
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    console.log(form.email, form.password);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <input type="text" value={form.email} onChange={formHandler} id="email" />
+        <input type="password" value={form.password} onChange={formHandler} id="password" />
+        <button>제출</button>
+      </form>
+    </>
+  );
+}
+
+export default UseInputCompo;
